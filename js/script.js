435f2b62bc6888c2ebc84ddbe72ab6962270ff2b
Katya Prymak <62486148+katyaprymak@users.noreply.github.com>
2024-01-07 15:40:07 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var igIcon = document.querySelector(".fa-instagram");
+igIcon.style.color = "#d45bb9";
+//console.log("igIcon");
+
+var fbIcon = document.querySelector(".fa-facebook");
+fbIcon.style.color = "#6a99b5";
+//console.log("fa-facebook");
+
+var hours = document.querySelector(".contact p");
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
